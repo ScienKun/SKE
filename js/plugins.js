6dc89726ce2a03aec7d47c4fce9849b81e0af356
ScienKun <scienkun68@gmail.com>
2018-01-21 18:50:23 +0100

    update files and game

--- js/plugins.js
+++ js/plugins.js
@@ -6,5 +6,5 @@ var $plugins =
 {"name":"--------------------","status":false,"description":"------------------------------------------------------------","parameters":{}},
 {"name":"SKS_TitleMap","status":true,"description":"v1.03 Utilise une map en tant qu'écran-titre pour votre jeu.\r\nPeut-être un tutoriel dessus un jour","parameters":{"ID MAP TITLE":"1"}},
 {"name":"SKS_Utils","status":true,"description":"v1.00 Script utilitaire.","parameters":{"textToQuit":"Back to Windows"}},
-{"name":"SKS_SleepStat","status":true,"description":"v1.0a Ajoute une nouvelle barre de stat.","parameters":{"slpText":"SLP","gaugeColor":"","gBC":"19","gHPC1":"7","gHPC2":"8","gMPC2":"8","gSlpC2":"8","variables":"","varMax":"1","varActu":"2"}}
+{"name":"SKS_SleepStat","status":true,"description":"v1.0a Ajoute une nouvelle barre de stat.","parameters":{"slpText":"Energie","gaugeColor":"","gBC":"19","gHPC1":"7","gHPC2":"8","gMPC2":"8","gSlpC2":"8","variables":"","varMax":"1","varActu":"2"}}
 ];
